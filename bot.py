5a50aeeab4487b16d2feec2147ebf3d787ec949e
davidrezende <davidpenalives@gmail.com>
2022-03-13 20:41:05 -0300

    update commands add;edit;show;delete

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import random
 from contextlib import nullcontext
 from datetime import datetime, timedelta
 from tkinter import Y
+from unicodedata import name
 
 import certifi
 import discord
@@ -44,6 +45,7 @@ async def addNote(ctx, nameNote: str, category: str, categoryText: str):
     else:
         await ctx.reply(saveNote(ctx, nameNote, category, categoryText))
 
+
 @bot.command(name='edit', help='Edit note. Ex: !edit <note> <category> <new_category_text>')
 async def editNote(ctx, nameNote: str, category: str, categoryText: str):
     print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}")
@@ -52,11 +54,23 @@ async def editNote(ctx, nameNote: str, category: str, categoryText: str):
     else:
         await ctx.reply(editNote(ctx, nameNote, category, categoryText))
 
+
+@bot.command(name='rm', help='Add note. Ex: !rm <note> <optional_category>')
+async def removeNote(ctx, nameNote: str, category: str = None):
+    print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}")
+    await ctx.reply(deleteNoteOrCategory(ctx, nameNote, category))
+
+
 @bot.command(name='show', help='Show note. Ex: !show <note>')
 async def showNote(ctx, nameNote: str):
     print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}")
     await findNoteByName(ctx, nameNote)
 
+    
+@bot.command(name='notes', help='Show yours notes. Ex: !notes')
+async def listNotes(ctx):
+    print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}")
+    await findNoteByName(ctx, nameNote)
 
 def myconverter(o):
     if isinstance(o, datetime):
@@ -64,11 +78,12 @@ def myconverter(o):
 
 
 async def findNoteByName(ctx, nameNote):
-    query = {"id_user": ctx.author.id, nameNote: {"$exists": True}}
+    query = {"id_user": ctx.author.id, "notes"+"."+nameNote: {"$exists": True}}
     note = collection.find_one(query)
     if(note):
-        onlyNote = note[nameNote]
-        onlyNote['dat_last_modified'] = json.dumps(onlyNote['dat_last_modified'], default=myconverter) if 'dat_last_modified' in onlyNote else 'none'
+        onlyNote = note['notes'][nameNote]
+        onlyNote['dat_last_modified'] = json.dumps(
+            onlyNote['dat_last_modified'], default=myconverter) if 'dat_last_modified' in onlyNote else 'none'
         onlyNote['dat_creation'] = json.dumps(
             onlyNote['dat_creation'], default=myconverter)
         onlyNote = str(onlyNote).replace("\"", "")
@@ -76,23 +91,22 @@ async def findNoteByName(ctx, nameNote):
     else:
         return await ctx.reply('> **' + nameNote + '** does not exists!')
 
-
 def embed(ctx, nameNote, response: str):
     response = response.replace("\'", "\"")
     response = json.loads(response)
-    
+
     embed = discord.Embed(
         title=nameNote.capitalize(),
         url="https://realdrewdata.medium.com/",
-        description="||"+
-        "\n> **Date creation=**  " + (response.get('dat_creation') if response.get('dat_creation') != None else "") + 
+        description="||" +
+        "\n> **Date creation=**  " + (response.get('dat_creation') if response.get('dat_creation') != None else "") +
         ("\n> **Date last modified=**  " + response.get('dat_last_modified') if response.get('dat_last_modified') != None else "") +
         "||",
         color=discord.Color.blue())
     embed.set_author(name="HaruBot", url="https://twitter.com/RealDrewDatad",
                      icon_url="https://i.pinimg.com/originals/b9/ee/eb/b9eeeb6c66879c91025108e8656b1ed0.jpg")
     # embed.set_author(name=ctx.author.display_name, url="https://twitter.com/RealDrewData", icon_url=ctx.author.avatar_url)
-    
+
     if not (response.get('thumbnail') is None):
         embed.set_thumbnail(url=response['thumbnail'])
     else:
@@ -101,7 +115,8 @@ def embed(ctx, nameNote, response: str):
     if not (response.get('image') is None):
         embed.set_image(url=response['image'])
     else:
-        embed.set_image(url="https://i.pinimg.com/originals/b9/ee/eb/b9eeeb6c66879c91025108e8656b1ed0.jpg")
+        embed.set_image(
+            url="https://i.pinimg.com/originals/b9/ee/eb/b9eeeb6c66879c91025108e8656b1ed0.jpg")
 
     for key, value in response.items():
         # if (key == "dat_creation"):
@@ -123,47 +138,78 @@ def embed(ctx, nameNote, response: str):
     embed.set_footer(text="About: haru-bot on Github")
     return embed
 
-def validateWhenAddOrUpdateFieldsEmbed(category: str, categoryText: str):
-    if category in ['image','thumbnail']:
-        print('validating embeded fields')
 
-        if categoryText.startswith("https://") and categoryText.__contains__(".") and (categoryText.index(".") - len(categoryText)) < 2:
+def validateWhenAddOrUpdateFieldsEmbed(category: str, categoryText: str):
+    if category in ['image', 'thumbnail']:
+        print('validating embeded fields')
+        if categoryText.startswith("https://") and categoryText.__contains__(".") and (len(categoryText) - categoryText.index(".")) > 2:
             return True
         else:
             return False
+    else:
+        return True
+
 
 def saveNote(ctx, nameNote, category, categoryText):
     queryUserExists = {"id_user": ctx.author.id}
     userExists = collection.count_documents(queryUserExists)
     if(userExists <= 0):
-        post = {"id_user": ctx.author.id, "dat_creation": datetime.today().replace(microsecond=0), nameNote: {
-            category: categoryText, "dat_creation": datetime.today().replace(microsecond=0)}}
+        post = {"id_user": ctx.author.id, "dat_creation": datetime.today().replace(microsecond=0), "notes": { nameNote: { category: categoryText, "dat_creation": datetime.today().replace(microsecond=0)}}}
         collection.insert_one(post)
-        return '> **' + ctx.author.name + ' congratulations!** You have created your first note: **' + nameNote + '**.\n+**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>'
+        return '> **' + ctx.author.name + ' congratulations!** You have created your first note: **' + nameNote + '**.\n**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>'
     else:
         queryNoteExists = {"id_user": ctx.author.id,
-                           nameNote: {"$exists": True}}
+                           "notes"+"."+nameNote: {"$exists": True}}
         noteExists = collection.count_documents(queryNoteExists)
         if (noteExists <= 0):
             filter = {"id_user": ctx.author.id}
-            newvalues = {"$set": {nameNote+'.'+category: categoryText, nameNote +
-                                  '.'+"dat_creation": datetime.today().replace(microsecond=0)}}
+            newvalues = {"$set": {"notes"+"."+nameNote+"."+category: categoryText, "notes"+"."+nameNote+"."+"dat_creation": datetime.today().replace(microsecond=0)}}
             collection.update_one(filter, newvalues)
             return '> ' + nameNote + ' saved.'
         else:
             queryNoteCategoryExists = {
-                "id_user": ctx.author.id, nameNote+'.'+category: {"$exists": True}}
+                "id_user": ctx.author.id, "notes"+"."+nameNote+"."+category: {"$exists": True}}
             noteCategoryExists = collection.count_documents(
                 queryNoteCategoryExists)
             if(noteCategoryExists <= 0):
                 filter = {"id_user": ctx.author.id,
-                          nameNote+'.'+category: {"$exists": False}}
-                newvalues = {"$set": {nameNote+'.'+category: categoryText, nameNote +
-                                      '.'+"dat_last_modified": datetime.today().replace(microsecond=0)}}
+                          "notes"+"."+nameNote+"."+category: {"$exists": False}}
+                newvalues = {"$set": {"notes"+"."+nameNote+"."+category: categoryText, "notes"+"."+nameNote+"."+"dat_last_modified": datetime.today().replace(microsecond=0)}}
                 collection.update_one(filter, newvalues)
                 return '> Note **' + nameNote + '** updated with new category **' + category + '**.'
             else:
-                return '> Your note **' + nameNote + '** already exists with category **'+ category +'**. \n**Suggestion:**\n!edit <note> <category> <text-from-category>'
+                return '> Your note **' + nameNote + '** already exists with category **' + category + '**. \n**Suggestion:**\n!edit <note> <category> <text-from-category>'
+
+
+def deleteNoteOrCategory(ctx, nameNote, category):
+    queryUserExists = {"id_user": ctx.author.id}
+    userExists = collection.count_documents(queryUserExists)
+    if(userExists <= 0):
+        return "> You don't have any notes created. Start by creating your first note.\n+**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>"
+    else:
+        queryNoteExists = {"id_user": ctx.author.id,
+                           "notes"+"."+nameNote: {"$exists": True}}
+        noteExists = collection.count_documents(queryNoteExists)
+        if (noteExists <= 0):
+            return '> **' + nameNote + '** does not exists!\n+**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>'
+        else:
+            if category is None:
+                filter = {"id_user": ctx.author.id}
+                newvalues = {"$unset":{"notes"+"."+nameNote:""}}
+                collection.update_one(filter, newvalues)
+                return 'Note **' + nameNote  + '** deleted.'
+            else:
+                queryNoteCategoryExists = {"id_user": ctx.author.id, "notes"+"."+nameNote+'.'+category: {"$exists": True}}
+                noteCategoryExists = collection.count_documents(
+                    queryNoteCategoryExists)
+                if(noteCategoryExists <= 0):
+                    return '> Category **' + category + '** does not exists.'
+                else:
+                    filter = queryNoteCategoryExists
+                    newvalues = {"$unset": {"notes"+"."+nameNote+'.'+category:""}}
+                    collection.update_one(filter, newvalues)
+                    return '> Category **' + category + '** deleted with success from **' + nameNote + '**.'
+
 
 def editNote(ctx, nameNote, category, categoryText):
     queryUserExists = {"id_user": ctx.author.id}
@@ -172,24 +218,23 @@ def editNote(ctx, nameNote, category, categoryText):
         return "> You don't have any notes created. \n**Helpful commands:** \n!add <note> <new_category> <category_text> \n!show <note>"
     else:
         queryNoteExists = {"id_user": ctx.author.id,
-                           nameNote: {"$exists": True}}
+                           "notes"+"."+nameNote: {"$exists": True}}
         noteExists = collection.count_documents(queryNoteExists)
         if (noteExists <= 0):
             return '> Note **'+nameNote+'** does not exists. \n**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>'
         else:
-            queryNoteCategoryExists = {
-                "id_user": ctx.author.id, nameNote+'.'+category: {"$exists": True}}
-            noteCategoryExists = collection.count_documents(
-                queryNoteCategoryExists)
+            queryNoteCategoryExists = {"id_user": ctx.author.id, "notes"+"."+nameNote+'.'+category: {"$exists": True}}
+            noteCategoryExists = collection.count_documents(queryNoteCategoryExists)
             if(noteCategoryExists <= 0):
                 return '> Your note **' + nameNote + '** does not have this category **' + category + '**. \n**Helpful commands:** \n!show <note>\n!add <note> <new_category> <category_text>'
             else:
                 filter = {"id_user": ctx.author.id,
-                          nameNote+'.'+category: {"$exists": True}}
-                newvalues = {"$set": {nameNote+'.'+category: categoryText, nameNote +
+                          "notes"+"."+nameNote+'.'+category: {"$exists": True}}
+                newvalues = {"$set": { "notes"+"."+nameNote+'.'+category: categoryText, "notes"+"."+nameNote +
                                       '.'+"dat_last_modified": datetime.today().replace(microsecond=0)}}
                 collection.update_one(filter, newvalues)
-                return '> Note **' + nameNote + '** updated with **success** with category **'+ category +'**! \n**Suggestion:** \n!show <note>'
+                return '> Note **' + nameNote + '** updated with **success** with category **' + category + '**! \n**Suggestion:** \n!show <note>'
+
 
 def save(ctx):
     myquery = {"_id": ctx.author.id}
